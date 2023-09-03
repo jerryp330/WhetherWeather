f7dd68d89a527f37549c522ddfbb571a6439b732
jerryp330 <110803469+jerryp330@users.noreply.github.com>
2023-09-03 16:58:06 -0600

    got the country api

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
+
+# API requests for weather and cities
+api_key = "e35a3b728a550599387fef9ebdd3ff21"
+country_reponse = requests.get("https://restcountries.com/v3.1/all?fields=name,flags,capital")
+
+if country_reponse.status_code == 200:
+    data = country_reponse.json()
+    print(data)
+
+def gen_city(): 
+    exit
+
 
 @app.route("/")
 def home():
@@ -12,3 +26,5 @@ def play():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
