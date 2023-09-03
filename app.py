c5a9a912349bc1e77de6e008c6309ec788c93fed
jerryp330 <110803469+jerryp330@users.noreply.github.com>
2023-09-03 17:11:38 -0600

    getting countryname, flag, and its capital

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import requests
+import random
 
 app = Flask(__name__)
 
@@ -7,12 +8,32 @@ app = Flask(__name__)
 api_key = "e35a3b728a550599387fef9ebdd3ff21"
 country_reponse = requests.get("https://restcountries.com/v3.1/all?fields=name,flags,capital")
 
-if country_reponse.status_code == 200:
-    data = country_reponse.json()
-    print(data)
 
 def gen_city(): 
-    exit
+    if country_reponse.status_code == 200:
+        data = country_reponse.json()
+    else:
+        return("Error: Failed to fetch from country API")
+
+    random_country = random.choice(data)
+    country_name = random_country.get("name", {}).get("common","")
+    flag_url = random_country.get("flags", {}).get("png", "")
+    capital_name = random_country.get("capital", "")
+
+    if country_name:
+        print(country_name)
+    else:
+        print("oops")
+    if flag_url:
+        print(flag_url)
+    else:
+        print("no flag")
+    if capital_name:
+        print(capital_name)
+    else: 
+        print("no capital")
+
+gen_city()
 
 
 @app.route("/")
@@ -26,5 +47,4 @@ def play():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
