516c3a551b23fd1d77a7a3035c1436676802d14f
jerryp330 <110803469+jerryp330@users.noreply.github.com>
2023-09-03 19:54:27 -0600

    fetched weather data

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 api_key = "e35a3b728a550599387fef9ebdd3ff21"
 country_reponse = requests.get("https://restcountries.com/v3.1/all?fields=name,flags,capital")
 
-# Retrieving data for the current country
+# Retrieving data for the current country. 
+# FUTURE IDEA: this api provides the continent for which the country is from, so I can make the multiple choice easier/harder
+# based on choosing how many options are from the same continent
 current_country = {      #stores the data of the random country obtained. Will be updated with the country's info.
     "name": "",
     "flag": "",
@@ -39,16 +41,36 @@ def gen_city():
     else: 
         current_country["capital"] = ""
 
+# Fetching data from the weather API for the current city's weather
+current_weather = {
+    "temp": "",
+    "humidity": "",
+    "sky": "",
+    "timezone": "",
+    #"name": ""
+}
+
+
 
 def get_weather():
-    weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q=Houston&units=imperial&APPID={api_key}")
-    if weather_data.status_code == 200:
-        print(weather_data.json())
-    else:
-        print("nooo")
+    gen_city()
 
-get_weather()
+    if current_country["capital"] != "":
+        weather_response = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={current_country['capital']}&units=metric&APPID={api_key}")
+    else:
+        return("Error fetching weather data due to no capital city.")
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+    else:
+        return("Error fetching weather data due to invalid status code.")
+
+    current_weather["temp"] = weather_data["main"]["temp"]
+    current_weather["humidity"] = weather_data["main"]["humidity"]
+    current_weather["sky"] = weather_data["weather"][0]["description"]
+    current_weather["timezone"] = weather_data["timezone"]
+    #current_weather["name"] = weather_data["name"]
+
 
 """
 @app.route("/")
